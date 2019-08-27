22490c2d5e0820a481cecbb9dd6e54e38cc2387c
Savke0203 <36578989+Savke0203@users.noreply.github.com>
2019-08-27 04:33:25 +0200

    Add files via upload

--- BMS/DAL/Database.php
+++ BMS/DAL/Database.php
@@ -1,27 +1,57 @@
 <?php 
 
-class Database {
+class Database
+{
+    /**
+     * @var string
+     */
+    private $hostname = 'localhost';
 
-private $hostname = 'localhost';
-private $username = 'root';
-private $password = '';
-private $db = 'bms';
+    /**
+     * @var string
+     */
+    private $username = 'root';
 
-public $conn;
+    /**
+     * @var string
+     */
+    private $password = '';
 
-function dbConnect() {
-	$this->conn = null;
-	try {
-		$this->conn = new PDO("mysql:host=" . $this->hostname . ";dbname=" . $this->db,
-			$this->username,$this->password);
-		$this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-		
-	} catch (PDOException $e) {
-		echo "Connect error " . $e->getMessage();
-	}
-	return $this->conn;
+    /**
+     * @var string
+     */
+    private $db = 'bms';
+
+    /**
+     * @var PDO|null
+     */
+    private $conn = null;
+
+    /**
+     * Get database connection.
+     *
+     * @return PDO
+     */
+    public function getConnection()
+    {
+        if (null === $this->conn) {
+            $this->dbConnect();
+        }
+
+        return $this->conn;
+    }
+
+    /**
+     * Establish database connection.
+     */
+    private function dbConnect()
+    {
+        try {
+            $this->conn = new PDO("mysql:host=" . $this->hostname . ";dbname=" . $this->db,
+                $this->username, $this->password);
+            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch (PDOException $e) {
+            die('Connect error ' . $e->getMessage());
+        }
+    }
 }
-
-} // end of class
-
- ?>
